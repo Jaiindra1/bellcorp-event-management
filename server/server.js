30b216525ae24b0211217307d94568d7410e5fcd
Jai indra <168549352+Jaiindra1@users.noreply.github.com>
2026-02-14 20:39:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,9 +14,11 @@ const app = express();
 // --------------------
 // Middleware
 // --------------------
-app.use(cors(
-   origin: "https://bellcorp-event-management-kappa.vercel.app/"
-));
+app.use(
+  cors({
+    origin: "https://bellcorp-event-management-kappa.vercel.app",
+  })
+);
 app.use(express.json());
 
 // --------------------
